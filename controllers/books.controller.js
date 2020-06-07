dce43c7efbaca471a6b5184d83c0cf9c69209c7e
HungUET231 <hunghungtstc@gmail.com>
2020-06-07 15:41:19 +0700

    fix model

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,63 +1,66 @@
 let Book = require('../models/books.model');
-let books = Book.find();
 
 module.exports.index = (req, res) => {
-  let page = parseInt(req.query.page) || 1;
-  const perPage = 5;
+    // let page = parseInt(req.query.page) || 1;
+    // const perPage = 5;
 
-  const startIndex = (page - 1) * perPage;
-  const endIndex = page * perPage;
-  
-  res.render("books", {
-    books: books.slice(startIndex, endIndex),
-    lastPage: Math.round(books.length/perPage)
-  });
+    // const startIndex = (page - 1) * perPage;
+    // const endIndex = page * perPage;
+    Book.find().then(function(books) {
+        res.render('books', {
+            books: books
+        });
+    });
+    // res.render("books", {
+    //     books //: books.slice(startIndex, endIndex),
+    //     // lastPage: Math.round(books.length/perPage)
+    // });
 };
 
 module.exports.search = (req, res) => {
-  let q = req.query.q;
-  let matchedBooks = books
-    .filter(function(item) {
-      return item.title.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    let q = req.query.q;
+    let matchedBooks = books
+        .filter(function(item) {
+            return item.title.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+        });
+    res.render("books", {
+        books: matchedBooks
     });
-  res.render("books", {
-    books: matchedBooks
-  });
 };
 
 module.exports.create = (req, res) => {
-  res.render("books/create");
+    res.render("books/create");
 };
 
 module.exports.postCreate = (req, res) => {
-  Book.create(req.body);
-  res.redirect("/books");
+    Book.create(req.body);
+    res.redirect("/books");
 };
 
 module.exports.view = (req, res) => {
-  let id = req.params.id;
-  let book = Book.findById(id);
-  res.render("books/view", {
-    book
-  });
+    let id = req.params.id;
+    let book = Book.findById(id);
+    res.render("books/view", {
+        book
+    });
 };
 
 module.exports.edit = (req, res) => {
-  let id = req.params.id;
-  let book = Book.findById(id);
-  res.render("books/edit", {
-    book
-  });
+    let id = req.params.id;
+    let book = Book.findById(id);
+    res.render("books/edit", {
+        book
+    });
 };
 
 module.exports.postEdit = (req, res) => {
-  let id = req.params.id;
-  let book = Book.findByIdAndUpdate(id);
-  res.redirect("/books");
+    let id = req.params.id;
+    let book = Book.findByIdAndUpdate(id);
+    res.redirect("/books");
 };
 
 module.exports.delete = (req, res) => {
-  let id = req.params.id;
-  Book.findByIdAndDelete(id);
-  res.redirect("/books");
+    let id = req.params.id;
+    Book.findByIdAndDelete(id);
+    res.redirect("/books");
 };
--- models/books.model.js
+++ models/books.model.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 let BookSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  cover: {
-    type: String,
-    default: ''
-  },
-  loves: Number,
-  genre: String
+    title: String,
+    description: String,
+    cover: {
+        type: String,
+        default: ''
+    },
+    loves: Number,
+    genre: String
 });
 
-let Book = mongoose.model('Books', BookSchema, 'books')
+let Book = mongoose.model('Book', BookSchema, 'books')
 
 module.exports = Book;
